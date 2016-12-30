d34643e0a42816ff0a02c998cbfa771288c60c5f
ddragosd <ddragosd@gmail.com>
2016-12-29 18:17:27 -0800

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -6,6 +6,10 @@
  */
 function main(params) {
     var name = params.name || params.payload || 'stranger';
+    if( params.context && params.context.identity ) {
+        name = params.context.identity.user_id;
+    }
+
     var place = params.place || 'branch-1';
     return {payload:  'Hello, ' + name + ' from ' + place + ' !'};
 }
