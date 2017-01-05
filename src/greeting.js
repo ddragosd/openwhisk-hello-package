fb531ca49f4d2e995f494bbad0edb1f7cb81708f
ddragosd <ddragosd@gmail.com>
2017-01-05 11:51:18 -0800

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -7,5 +7,5 @@
 function main(params) {
     var name = params.name || params.payload || 'stranger';
     var place = params.place || 'somewhere';
-    return {payload:  'Hello, ' + name + ' from ' + place + ' !'};
+    return {payload:  'Hello, ' + name + ' from demo-1 branch !'};
 }
