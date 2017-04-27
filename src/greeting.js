b98db930adfe4a10708988f5e626287d61d0424f
ddragosd <ddragosd@gmail.com>
2017-04-27 09:11:10 -0500

    Update greeting.js

--- src/greeting.js
+++ src/greeting.js
@@ -14,7 +14,7 @@ function main(params) {
         name = params.context.identity.user_id;
     }
 
-    var place = params.place || 'branch....1..apr-20';
+    var place = params.place || 'Serverless Conf';
     return {
         payload:  'Hello, ' + name + ' from ' + place + ' !',
         event: params
